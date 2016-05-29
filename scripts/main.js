0be7d863fc4c01c3851b809c11f8b3ec623fe17b
Iurii Koval <yuriy.kowal@gmail.com>
2016-05-30 00:38:32 +0300

    add button names loading

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,47 @@
+function addFilmsButtons(starWarsEntities, onSuccessCallback, onFailureCallback) {
+                var url = "http://swapi.co/api/"+starWarsEntities;
+                var request = new XMLHttpRequest();    
+                request.onreadystatechange = function() {
+                    if(request.readyState == 4 && request.status == 200) {
+                      var entities = request.response.results;
+                      onSuccessCallback(entities);
+                    } else {
+                        document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait ...";
+                    }
+                }
+                request.open("GET", url, true);
+                request.responseType="json";
+                request.send(); 
+            }
+
+function addButtons(starWarsEntities) {
+                var url = "http://swapi.co/api/"+starWarsEntities;
+                var request = new XMLHttpRequest();    
+                request.onreadystatechange = function() {
+                    if(request.readyState == 4 && request.status == 200) {
+                      var entities = request.response.results;
+                      loadContent(entities);
+                    } else {
+                        document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait ...";
+                    }
+                }
+                request.open("GET", url, true);
+                request.responseType="json";
+                request.send(); 
+            }
+
+function loadContent(entities) {
+                for(i=0;i<entities.length;i++){
+                    var buttonName = entities[i].name;
+                    console.log(buttonName);
+                    var buttonTemplate = "<input type='button' value={{name}}>";
+                    var template = Handlebars.compile(buttonTemplate);
+                    var button = template({name:buttonName});
+                    document.getElementById("buttonsContainer").innerHTML += button;
+                }
+                document.getElementById("detailedInfo").innerHTML = "";
+            }
+
 function getDetailedInfo(section, itemId) {
 
     var request = new XMLHttpRequest();    
