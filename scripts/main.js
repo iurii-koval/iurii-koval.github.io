a64b06d2e04a2330ffaa95f753d1fbdeacb620c9
Iurii Koval <yuriy.kowal@gmail.com>
2016-06-05 15:18:45 +0300

    added info loading for each entity

--- scripts/main.js
+++ scripts/main.js
@@ -1,72 +1,61 @@
 function addFilmsButtons(starWarsEntities, onSuccessCallback, onFailureCallback) {
-                var url = "http://swapi.co/api/"+starWarsEntities;
-                var request = new XMLHttpRequest();    
-                request.onreadystatechange = function() {
-                    if(request.readyState == 4 && request.status == 200) {
-                      var entities = request.response.results;
-                      onSuccessCallback(entities);
-                    } else {
-                        document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait ...";
-                    }
-                }
-                request.open("GET", url, true);
-                request.responseType="json";
-                request.send(); 
-            }
+    var url = "http://swapi.co/api/" + starWarsEntities;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            var entities = request.response.results;
+            onSuccessCallback(entities);
+        } else {
+            document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait ...";
+        }
+    }
+    request.open("GET", url, true);
+    request.responseType = "json";
+    request.send();
+}
 
 function addButtons(url) {
-                var request = new XMLHttpRequest();    
-                request.onreadystatechange = function() {
-                    if(request.readyState == 4 && request.status == 200) {
-                      var entities = request.response.results;
-                      loadContent(entities);
-                        if(null !=request.response.next){
-                            var nextUrl = request.response.next;
-                            var loadMoreButton = "<input type='button' value='Load more' onclick='addButtons(\""+nextUrl+"\")'>"
-                            eraseContent("loadMore");
-                            document.getElementById("loadMore").innerHTML += loadMoreButton; 
-                        } else {
-                            eraseContent("loadMore");
-                        }
-                    } else {
-                        document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait ...";
-                    }
-                }
-                request.open("GET", url, true);
-                request.responseType="json";
-                request.send(); 
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            var entities = request.response.results;
+            loadContent(entities);
+            if (null != request.response.next) {
+                var nextUrl = request.response.next;
+                var loadMoreButton = "<input type='button' value='Load more' onclick='addButtons(\"" + nextUrl + "\")'>"
+                eraseContent("loadMore");
+                document.getElementById("loadMore").innerHTML += loadMoreButton;
+            } else {
+                eraseContent("loadMore");
             }
-function eraseContent(elementsId) {
-    document.getElementById(elementsId).innerHTML ="";
+        } else {
+            document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait ...";
+        }
+    }
+    request.open("GET", url, true);
+    request.responseType = "json";
+    request.send();
 }
+
+function eraseContent(elementsId) {
+    document.getElementById(elementsId).innerHTML = "";
+}
+
 function loadContent(entities) {
-                for(i=0;i<entities.length;i++){
-                    var buttonName = entities[i].name;
-                    var entity = entities[i];
-                    //console.log("entity: " + JSON.stringify(entities[i]));
-                    //var buttonTemplate = "<input type='button' value='{{name}}'>";
-                    var buttonTemplate = "<input type='button' value='{{name}}' onclick='{loadFunction}'>";
-                    var fun = "showDetailedInfo("+entity+")";
-                    var template = Handlebars.compile(buttonTemplate);
-                    var button = template({name:buttonName, loadFunction:fun});
-                    document.getElementById("buttonsContainer").innerHTML += button;
-                }
-                document.getElementById("detailedInfo").innerHTML = "";
-            }
+    for (i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        var buttonName = entity.name;
+        var info = JSON.stringify(entity);
+        var buttonTemplate = "<input type='button' value='{{name}}' onclick='showDetailedInfo(" + info + ")'>";
+        var template = Handlebars.compile(buttonTemplate);
+        var button = template({
+            name: buttonName
+        });
+        document.getElementById("buttonsContainer").innerHTML += button;
+    }
+    document.getElementById("detailedInfo").innerHTML = "";
+}
 
 function showDetailedInfo(entity) {
     document.getElementById("detailedInfo").innerHTML = JSON.stringify(entity);
-}
-function getDetailedInfo(section, itemId) {
-
-    var request = new XMLHttpRequest();    
-    request.onreadystatechange = function() {
-            if(request.readyState == 4 && request.status == 200){
-            document.getElementById("detailedInfo").innerHTML = request.responseText;
-        } else {
-            document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait.";
-        }
-    }
-    request.open("GET", "http://swapi.co/api/" + section + "/" + itemId + "/", true);
-    request.send();    
 }
