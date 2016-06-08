c0178356d5534a8cbcdd53f7ea74c5dee3786094
Coder_01 <you@example.com>
2016-06-08 15:36:50 +0200

    added table

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,3 @@
-function addFilmsButtons(starWarsEntities, onSuccessCallback, onFailureCallback) {
-    var url = "http://swapi.co/api/" + starWarsEntities;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            var entities = request.response.results;
-            onSuccessCallback(entities);
-        } else {
-            document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait ...";
-        }
-    }
-    request.open("GET", url, true);
-    request.responseType = "json";
-    request.send();
-}
-
 function addButtons(url, property) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -23,7 +7,6 @@ function addButtons(url, property) {
             if (null != request.response.next) {
                 var nextUrl = request.response.next;
                 var loadMoreButton = "<input type='button' value='Load more' onclick='addButtons(\"" + nextUrl +"\",\"" + property+"\")'>"
-                console.log("button: " + loadMoreButton);
                 eraseContent("loadMore");
                 document.getElementById("loadMore").innerHTML += loadMoreButton;
             } else {
@@ -47,7 +30,8 @@ function loadContent(entities, property) {
         var entity = entities[i];
         var buttonName = entity[property];
         var info = JSON.stringify(entity);
-        var buttonTemplate = "<input type='button' value='{{name}}' onclick='showDetailedInfo(" + info + ")'>";
+        //var buttonTemplate = "<input type='button' value='{{name}}' onclick='showDetailedInfo(\"" + info + "\")'>";
+        var buttonTemplate = "<input type='button' value='{{name}}' onclick='createTableWithData(" + info + ")'>";
         var template = Handlebars.compile(buttonTemplate);
         var button = template({
             name: buttonName
@@ -57,6 +41,30 @@ function loadContent(entities, property) {
     document.getElementById("detailedInfo").innerHTML = "";
 }
 
-function showDetailedInfo(entity) {
-    document.getElementById("detailedInfo").innerHTML = JSON.stringify(entity);
+function createTableWithData(entity) {
+    var table = document.createElement("table");
+    //table.style.display = "block";
+    table.style.width = '30%';
+    
+    table.setAttribute("border", "1");
+    var thead = document.createElement("th");
+    thead.appendChild(document.createTextNode(entity[Object.keys(entity)[0]]));
+    
+    var tbody = document.createElement("tbody");
+    for(var property in entity) {
+        if(entity.hasOwnProperty(property)) {            
+        var tr = document.createElement("tr");
+        var key = document.createElement("td");
+        key.appendChild(document.createTextNode(property));
+        var value = document.createElement("td");
+        value.appendChild(document.createTextNode(entity[property]));
+        tr.appendChild(key);
+        tr.appendChild(value);            
+        tbody.appendChild(tr);
+        }
+    }
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    document.getElementById("detailedInfo").innerHTML = "";
+    document.getElementById("detailedInfo").appendChild(table);
 }
